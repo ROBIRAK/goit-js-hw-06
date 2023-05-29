141d4fccd7879c7ce298a8e075f8088b96e6d908
ROBIRAK <115900129+ROBIRAK@users.noreply.github.com>
2023-05-29 08:40:40 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('form.login-form');
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const emailInput = event.currentTarget.elements.email;
+  const passwordInput = event.currentTarget.elements.password;
+  
+  if (emailInput.value === '' || passwordInput.value === '') {
+    alert('Всі поля повинні бути заповнені!');
+  } else {
+    console.log({
+      email: emailInput.value,
+      password: passwordInput.value,
+    });
+    
+    event.currentTarget.reset();
+  }
+});
